49c7334b0758f6bf994d269c7b9d868230790fc5
binhnguyen96html <binhnguyenbiz96@gmail.com>
2024-05-08 21:33:08 -0700

    complete project

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('hi')
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { UserProps, User } from './models/User';
+
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
+
+users.on('change', () => {
+  const root = document.getElementById('root')
+
+  if(root){
+    new UserList(root, users).render()
+  }
+})
+
+users.fetch()
